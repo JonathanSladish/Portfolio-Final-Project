e478991e09fd532db7b16ac590cdbd1c3d65c8dd
JonathanSladish <75808482+JonathanSladish@users.noreply.github.com>
2021-03-16 11:42:16 -0500

    Initial commit

---
+++ src/main/java/com/Portfolio/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.Portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping(value = "/")
+	public String home() {
+		return "main";
+	}
+
+
+	@GetMapping(value = "/about")
+	public String about() {
+		return "about";
+	}
+		@GetMapping(value = "/contact")
+		public String contact() {
+			return "contact";
+		}
+			@GetMapping(value = "/work")
+			public String work() {
+				return "work";
+	}
+}
---
+++ src/test/java/com/Portfolio/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
