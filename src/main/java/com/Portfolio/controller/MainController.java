c906b94ca377bf629a59c4b9416d5f8460d2fe04
JonathanSladish <75808482+JonathanSladish@users.noreply.github.com>
2021-03-18 09:30:33 -0500

    4 update

--- src/main/java/com/Portfolio/controller/MainController.java
+++ src/main/java/com/Portfolio/controller/MainController.java
@@ -6,22 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-	@GetMapping(value = "/")
-	public String home() {
-		return "main";
-	}
+    @GetMapping(value = "/")
+    public String home() {
+        return "main";
+    }
 
 
-	@GetMapping(value = "/about")
-	public String about() {
-		return "about";
+    @GetMapping(value = "/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping(value = "/resume")
+	public String resume() {
+		return "resume";
 	}
-		@GetMapping(value = "/contact")
-		public String contact() {
-			return "contact";
-		}
-			@GetMapping(value = "/work")
-			public String work() {
-				return "work";
-	}
+
+    @GetMapping(value = "/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping(value = "/work")
+    public String work() {
+        return "work";
+    }
 }
